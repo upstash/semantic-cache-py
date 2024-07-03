98ddcf1d57dff351a7ea6f5ede48cf1f09a77fc3
Eray Eroğlu <erayeroglu07@gmail.com>
2024-07-04 01:42:21 +0300

    dependency update

--- semantic_cache/semantic_caching.py
+++ semantic_cache/semantic_caching.py
@@ -46,7 +46,8 @@ class SemanticCache:
     ) -> Optional[List[Generation]]:
         """
         Converts the JSON string to generations and returns them.
-
+        It is called by Langchain to check if the generations are in the cache.
+        
         Args:
             prompt (str): The prompt to lookup in the cache.
             llm_string (Optional[str]): Optional string for LLM context.
@@ -65,7 +66,8 @@ class SemanticCache:
     ) -> None:
         """
         Converts the generations to a JSON string and stores it in the cache.
-
+        It is called by Langchain to upload the generations to the cache.
+        
         Args:
             prompt (str): The prompt to update in the cache.
             llm_string (Optional[str]): Optional string for LLM context.
